65832fc9c005e6da67e6fe1b8270b512d1c4e305
TemurWebDev <temurbekqurbonov01@gmail.com>
2023-02-23 02:50:13 +0500

    deploy

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -53,20 +53,30 @@ async def bot_start(message: types.Message, state: FSMContext):
 
 @dp.message_handler(state='reklama')
 async def send_ad_to_all(message: types.Message,state: FSMContext):
-    await bot.send_message(chat_id=ADMINS[0],text=f"Habar to'g'rimi ‼️\n{message.text}",reply_markup=rek)
-    @dp.callback_query_handler(text="ha",state='reklama')
-    async def rek_ha(call: CallbackQuery):
-        users = db.select_all_users()
-        for user in users:
-            user_id = user[1]
-            await bot.send_message(chat_id=user_id, text=f"{message.text}")
-            await asyncio.sleep(0.05)
-        await bot.send_message(chat_id=ADMINS[0],text=f"Reklama yuborildi! ✅")
-        await state.finish()
-        await call.message.delete()
-    @dp.callback_query_handler(text="yuq",state='reklama')
-    async def rek_yuq(call: CallbackQuery):
-        await bot.send_message(chat_id=ADMINS[0],text="Reklama yuborilmadi! ❌")
-        await state.finish()
-        await call.message.delete()
+    try:
+        await bot.send_message(chat_id=ADMINS[0],text=f"Habar to'g'rimi ‼️\n{message.text}",reply_markup=rek)
+        try:
+            @dp.callback_query_handler(text="ha",state='reklama')
+            async def rek_ha(call: CallbackQuery):
+                users = db.select_all_users()
+                for user in users:
+                    user_id = user[1]
+                    try:
+                        await bot.send_message(chat_id=user_id, text=f"{message.text}")
+                    except Exception as e:
+                        print(e)
+                    await asyncio.sleep(0.05)
+                await bot.send_message(chat_id=ADMINS[0],text=f"Reklama yuborildi! ✅")
+                await state.finish()
+                await call.message.delete()
+        except Exception as e:
+            print(e)
+        @dp.callback_query_handler(text="yuq",state='reklama')
+        async def rek_yuq(call: CallbackQuery):
+            await bot.send_message(chat_id=ADMINS[0],text="Reklama yuborilmadi! ❌")
+            await state.finish()
+            await call.message.delete()
+    except Exception as e:
+        print(e)
+    
 
